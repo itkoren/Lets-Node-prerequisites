ae553c5ae1633500804197ff19f630f442598a7d
Itai Koren <itkoren@gmail.com>
2014-05-24 22:19:08 +0300

    some small fixes
    Signed-off-by: Itai Koren <itkoren@gmail.com>

--- tcpserver.js
+++ tcpserver.js
@@ -3,6 +3,4 @@ var net = require('net');
 var server = net.createServer(function (socket) {
     socket.write('Echo server\r\n');
     socket.pipe(socket);
-});
-
-server.listen(1337, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
